4708accc02a85bbb041b8f0e0be67c2a525e0d1f
Kristian Borgwarth <10348902@pm.me>
2026-03-24 21:36:17 +0100

    feat(persistence): note / tag repository

--- persistence/repositories/note_repository.go
+++ persistence/repositories/note_repository.go
@@ -10,3 +10,19 @@ type noteRepository struct {
 	db *sql.DB
 }
 
+func NewNoteRepository(db *sql.DB) NoteRepository {
+	return &noteRepository{db: db}
+}
+
+
+func (r *noteRepository) Upsert(title string, path string, slug string) error {
+	query := `
+	INSERT INTO notes (title, path, slug)
+	VALUES ($1, $2, $3)
+	ON CONFLICT (slug) DO UPDATE
+	SET title = EXCLUDED.title,
+	    path = EXCLUDED.path;
+	`
+	_, err := r.db.Exec(query, title, path, slug)
+	return err
+}
--- persistence/repositories/tag_repository.go
+++ persistence/repositories/tag_repository.go
@@ -4,7 +4,7 @@ package repositories
 import "database/sql"
 
 type TagRepository interface {
-	Upsert(title string, path string, slug string) error
+	Upsert(title string, path string) error
 }
 
 type tagRepository struct {
@@ -15,3 +15,14 @@ func NewTagRepository(db *sql.DB) TagRepository {
 	return &tagRepository{db: db}
 }
 
+func (r *tagRepository) Upsert(title string, path string) error {
+	query := `
+	INSERT INTO tags (title, path)
+	VALUES ($1, $2)
+	ON CONFLICT (slug) DO UPDATE
+	SET title = EXCLUDED.title,
+	    path = EXCLUDED.path;
+	`
+	_, err := r.db.Exec(query, title, path)
+	return err
+}
