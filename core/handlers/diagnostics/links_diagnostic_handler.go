a4306b089eeddf2f1fa50c4f71600c8fdb0900a2
Kristian Borgwarth <10348902@pm.me>
2026-05-09 15:46:03 +0200

    feat(diagnostic/link): retrive broken links

---
+++ core/handlers/diagnostics/links_diagnostic_handler.go
@@ -0,0 +1,64 @@
+package diagnostics
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/KristianJBorgwarth/dendrite.daemon/core/dtos"
+	"github.com/KristianJBorgwarth/dendrite.daemon/core/models"
+	"github.com/KristianJBorgwarth/dendrite.daemon/persistence/repositories"
+)
+
+type diagnoseNoteCommand struct {
+	Path string `json:"path"`
+}
+
+type DiagnoseNoteHandler struct {
+	noteRepository repositories.INoteRepository
+	linkRepository repositories.ILinkRepository
+}
+
+func NewDiagnoseNoteHandler(
+	noteRepository repositories.INoteRepository,
+	linkRepository repositories.ILinkRepository,
+) *DiagnoseNoteHandler {
+	return &DiagnoseNoteHandler{noteRepository, linkRepository}
+}
+
+func (h *DiagnoseNoteHandler) Handle(ctx context.Context, raw json.RawMessage) (any, error) {
+	var cmd diagnoseNoteCommand
+
+	if err := json.Unmarshal(raw, &cmd); err != nil {
+		return nil, err
+	}
+
+	note, err := h.noteRepository.GetByPath(ctx, cmd.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	brokenLinks, err := h.linkRepository.GetBrokenLinks(ctx, note.ID())
+	if err != nil {
+		return nil, err
+	}
+
+	return mapToDiagnosticResult(note, brokenLinks), nil
+}
+
+func mapToDiagnosticResult(note *models.Note, links []*models.Link) []*dtos.LinkDiagnosticDto {
+	var diagnostics []*dtos.LinkDiagnosticDto
+
+	for _, link := range links {
+		diagnostics = append(diagnostics, &dtos.LinkDiagnosticDto{
+			NoteID:   link.ID(),
+			NotePath: note.Path(),
+			Line:     link.Line(),
+			Col:      link.Col(),
+			Raw:      link.Raw(),
+			Target:   link.TargetSlug(),
+			Message:  "Broken link: target note does not exist",
+		})
+	}
+
+	return diagnostics
+}
